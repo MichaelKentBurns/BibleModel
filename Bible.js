a1470387e7941fac441194860ac43bc2cd41496a
MichaelKentBurns <MichaelBurns@mac.com>
2024-08-29 17:19:09 -0500

    New state machine.  Runs loads from SQL and saves as JSON

--- Bible.js
+++ Bible.js
@@ -1,21 +1,31 @@
-let traceBible = true;
+const traceBible = true;
+var state;
+let                             state_init = 1, state_loadBooks = 2, state_waitBooks = 3, state_booksLoaded = 4, state_saveBooks = 5, state_booksSaved = 6, state_whatsNext = 7;
+const stateNames = [ 'null', 'init',        'loadBooks',          'waitBooks',         'booksLoaded',         'saveBooks',         'booksSaved',         'whatsNext' ];
+var stateNext;
+var iteration;
+if ( state == undefined ){
+    state = state_init;
+    iteration = 1;
+    if ( traceBible ) console.log('Bible.js Initializing for the first time');
+}  else {
+    iteration += 1;
+    if ( traceBible ) console.log('Bible.js iteration ', iteration, ' state=', state, ':', stateNames[state] );
+}
+
 if ( traceBible ) console.log('Bible.js Initializing');
 let Book = require('./Book.js');
 // let Location = require('./Location.js');
-let Xref           =require('./Xref.js');
-let theBible;
+let Xref = require('./Xref.js');
 
-let waitBooksStop;
-function waitBooks() {
-    if ( traceBible ) console.log('Waiting for all Books. There are ', theBible.books.length, ' books loaded.');
-    if (theBible.booksComplete) {
-        if ( traceBible ) console.log('waitBooks: All books are loaded.');
-        return true;
-        clearInterval(waitBooksStop);
-    }
-    else return false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+let theBible;
+
+//- - - - - - - - - - - begin Class definition - - - - - - - - - - -
 class Bible {
     constructor() {
         this.books = [];
@@ -37,49 +47,128 @@ class Bible {
         if ( traceBible ) console.log('Xref added: ' + xref);
     }
 
-
     LoadAll() {
         if ( traceBible ) console.log('Loading books...');
         this.booksComplete = false;
         Book.load(theBible);
-
-    //    Book.saveAll(theBible);
-    //    console.log("Books saved.");
 
         console.log('Loading cross references...');
         // Xref.load(theBible);
         if (theBible.xrefs != undefined) {
             if ( traceBible ) console.log('There are ', theBible.xrefs.length, ' cross references');
         }
+    }
+// if ( traceBible ) console.log('Bible Class Defined.');
+}
+//- - - - - - - - - - -end Class definition - - - - - - - - - - -
 
+if ( state == state_init ) {
+    theBible = new Bible();
+    if ( traceBible )     console.log('theBible created.');
+    Book.setBible(theBible);
+    theBible.bookInitialized = true;
+    Xref.setBible(theBible);
+    theBible.xrefInitialized = true;
 
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state]);
+}
+
+if ( state == state_loadBooks ) {
+    if (traceBible) console.log('Loading books...');
+    theBible.LoadAll();
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+}
+
+async function waitForBooks(){
+    let timeoutCount = 10;
+    waitBooksStop = setInterval(waitBooks, 1000);
+    while ( !waitBooks() && timeoutCount > 0 && !theBible.booksComplete ) {
+        if ( traceBible ) console.log('Still waiting for books to be done loading, ', timeoutCount, ' seconds to go.');
+        timeoutCount--;
+        await new Promise( r=> setTimeout(r, 1000 ));
     }
 }
-if ( traceBible ) console.log('Bible Class Defined.');
+
+let waitBooksStop;
+function waitBooks() {
+    if ( traceBible ) console.log('Waiting for all Books. There are ', theBible.books.length, ' books loaded.');
+    if (theBible.booksComplete) {
+        if ( traceBible ) console.log('waitBooks: All books are loaded.');
+        clearInterval(waitBooksStop);
+        stateNext = 0;
+        state += 1;
+        if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+        console.log('Might as well try to save now...');
+        Book.saveAll(theBible);
+        console.log("Books saved??");
+        stateNext = 0;
+        state += 1;
+        if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+        return true;
+    }
+    else
+        return false;
+}
+
+if ( state == state_waitBooks ) {
+    if (traceBible) console.log('Waiting for books to be done loading.')
+    waitForBooks();
+    if (traceBible) console.log('There are ', theBible.books.length, ' books loaded.');
+    if (traceBible) console.log('We think they must be loaded.  Are they?')
+    if (traceBible) console.log('Done.');
+    stateNext = 0;
+//    state += 1;
+//    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+
+}
 
 
-    theBible = new Bible();
-if ( traceBible )     console.log('theBible created.');
-Book.setBible(theBible);
-theBible.bookInitialized = true;
-Xref.setBible(theBible);
-theBible.xrefInitialized = true;
+if ( state == state_booksLoaded ) {
+    if (traceBible) console.log('Books are done loading.')
 
-if ( theBible.bookInitialized && theBible.xrefInitialized ) {
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+}
 
-    theBible.LoadAll();
-    if ( traceBible )     console.log('Waiting for books to be done loading.')
-    waitBooksStop = setInterval(waitBooks, 1000);
-    while ( waitBooks() == false ) {
-        if ( traceBible ) console.log('Still waiting for books to be done loading.')}
-    if ( traceBible ) console.log('There are ', theBible.books.length, ' books loaded.');
 
-    if ( traceBible ) console.log('We think they must be loaded.  Are they?')
+if ( state == state_saveBooks ) {
+    if (traceBible) console.log('Saving books as json.')
 
-    if ( traceBible ) console.log('Done.');
+    Book.saveAll(theBible);
+    console.log("Books saved??");
+
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+}
+
+
+if ( state == state_booksSaved ) {
+    if (traceBible) console.log('Now books are saved, now what?.')
+    console.log("Books saved!");
+
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
 }
 
 this.bibleInitialized = true;
-if ( traceBible ) console.log('Bible.js Complete.');
+if ( traceBible ) console.log('Bible.js Complete state=', state, ':', stateNames[state], ' stateNext=', stateNext, ':' , stateNames[stateNext]);
+if ( stateNext > 0 ) {
+    state = stateNext;
+}
+else {
+    console. log('start nextTick');
+    process.nextTick(() => {
+        console. log('nextTick callback');
+        });
+    console. log('scheduled');
+}
+
 
 module.exports = Bible, theBible;
---
+++ Main.js
@@ -0,0 +1,258 @@
+let theBible;
+
+const DataSource = require('./DataSource.js');
+const fs = require('fs');
+//------------------------------------------------------------------------------------------
+class Bible {
+    constructor() {
+        this.books = [];
+        this.xrefs = [];
+    }
+
+    AddBook(book) {
+        this.books.push(book);
+        console.log('Book added: ' + book.name);
+    }
+
+    AddXref(xref) {
+        this.xrefs.push(xref);
+        console.log('Xref added: ' + xref);
+    }
+
+    LoadAll() {
+
+        console.log('Loading books...');
+        let row = new Object();
+        let protoBook = new Book(row);
+        Book.load(theBible);
+        if (theBible.books != undefined) {
+            console.log('There are ', theBible.books.length, ' books loaded.');
+        }
+
+        Book.saveAll(theBible);
+        console.log("Books saved.");
+
+        console.log('Loading cross references...');
+        Xref.load(theBible);
+        if (theBible.xrefs != undefined) {
+            console.log('There are ', theBible.xrefs.length, ' cross references');
+        }
+
+    }
+}
+
+
+//------------------------------------------------------------------------------------------
+
+const million     	= 1_000_000;
+const allVerses     	=       999;
+const allChapters   	=   999_999;
+const thousand 	  	=     1_000;
+
+function idToBPath(id){
+	let millions =  Math.trunc( id / million );
+	let thousands = Math.trunc(  ( id % million ) / thousand );
+	let units = Math.trunc( id % thousand );
+	let path = [];
+	path[0] = millions;
+	path[1] = thousands;
+	path[2] = units;
+	return path;
+}
+
+
+class Location {
+
+    constructor(name,id) {
+        this.name 	= name;
+        this.id 	= id;
+		this.path	= idToBPath(id);
+	}
+
+	path() {
+		return this.path;
+	}
+	cpath(id){
+		let millions =  Math.trunc( id / million );
+		let thousands = Math.trunc(  ( id % million ) / thousand );
+		let units = Math.trunc( id % thousand );
+		let path = [];
+		path[0] = millions;
+		path[1] = thousands;
+		path[2] = units;
+		return path;
+	}
+}
+
+
+//------------------------------------------------------------------------------------------
+class Passage {
+
+    constructor( name, startLoc, endLoc) {
+        this.name 	= name;
+        this.startId 	= startLoc.id ;
+		this.startPath	= idToBPath(startId);
+        this.endId 	= endLoc.id;
+		this.endPath	= idToBPath(endId);
+	}
+
+	name() {
+		return this.name;
+	}
+	startId() {
+		return this.startId;
+	}
+	endId() {
+		return this.endId;
+	}
+
+	startPath() {
+		return this.startPath;
+	}
+	endPath() {
+		return this.endPath;
+	}
+}
+
+//------------------------------------------------------------------------------------------
+Location.locationTest = function() {
+	console.log('locationTest start.');
+	const Genesis = 1_000_000,
+      	Gen_1_1 = 1_001_001,
+      	Matthew = 40_000_000,
+      	Matt_1_1 = 40_001_001,
+      	Matt_28_18 = 40_028_018
+      	;
+      
+	path1 = idToBPath(Genesis);		 console.log('Genesis = ', path1);
+	path2 = idToBPath(Matt_28_18);		 console.log('GreatCommission = ', path2);
+	
+	genesisStart	= Location('Genesis', Genesis);
+		console.log('genesisStart: ' + genesisStart );
+	genesisEnd   	= Location('Genesis end', Genesis+AllChapters+AllVerses);
+		console.log('genesisEnd: ' + genesisEnd );
+	genesisAll  	= Passage ('Genesis all', genesisStart, genesisEnd);
+		console.log('genesisAll: ' + genesisAll );
+	
+	console.log('locationTest end.');
+       }
+
+
+//------------------------------------------------------------------------------------------
+class Book {
+    constructor(row) {
+        this.ordinal     = row.order;
+        this.name        = row.title_short;
+        this.title       = row.title_full;
+        this.category    = row.category;
+        this.nChapters   = row.chapters;
+    }
+
+}
+    Book.load = function ( theBible ) {
+        data = new DataSource();
+        DataSource.open();
+let books = [];
+        data.db.serialize(function () {
+            data.db.each('SELECT * FROM "book_info"', function (err, row) {
+                if (err) console.log(' Error: ', err);
+                book = new Book(row);
+                theBible.AddBook(book);
+                books.push(book);
+                console.log(`Book[${book.ordinal}] ${book.name} loaded.`)
+                console.log(book);
+            });
+        });
+            data.db.close();
+            console.log(theBible.books.length + " books loaded.");
+            Book.saveAll(theBible);
+            console.log("done load and save.");
+    }
+
+    Book.saveAll = function (theBible) {
+
+    if ( theBible != undefined && theBible.books != undefined) {
+        console.log(theBible.books);
+
+        // convert JSON object to a string
+        const jsonData = JSON.stringify(theBible.books)
+
+        // write JSON string to a file
+        fs.writeFile('books.json', jsonData, err => {
+            if (err) {
+                throw err
+            }
+            console.log('Books JSON data is saved.')
+        })
+
+    }
+
+}
+
+
+let nXrefs = 0;
+let announceInterval = 1_000;
+class Xref {
+    constructor(row) {
+        let path;
+        this.sourceId               = row.vid;
+        this.sourceLoc              = new Location('source', this.sourceId)
+        path 			            = this.sourceLoc.path;
+        this.sourceBook             = path[0];
+        this.sourceChapter          = path[1];
+        this.sourceVerse            = path[2];
+        this.rank                   = row.rank;
+        this.targetId               = row.sv;
+        this.targetLoc              = new Location('target', this.targetId);
+        path 			            = this.targetLoc.path;
+        this.targetBook             = path[0];
+        this.targetChapter          = path[0];
+        this.targetVerse            = path[0];
+        this.targetEndId            = row.ev;
+        if ( this.targetEndId != 0)
+        {
+            this.targetEndLoc       = new Location('targetEnd', this.targetEndId)
+            path 	                = this.targetEndLoc.path;
+            this.targetEndBook      = path[0];
+            this.targetEndChapter   = path[1];
+            this.targetEndVerse     = path[2];
+        }
+        this.type        = 0;
+    }
+}
+
+    Xref.load = function (theBible) {
+
+    DataSource.open();
+    let order = 0;
+    data.db.serialize(function () {
+        data.db.each('SELECT * FROM "cross_reference"', function (err, row) {
+            if (err) console.log(' Error: ', err);
+            let xref = new Xref(row);
+            theBible.AddXref(xref);
+            console.log(xref);
+            order ++;
+            nXrefs++;
+            if ( nXrefs % announceInterval == 0 ) {
+                console.log('There are now ', nXrefs, ' cross references loaded.');
+                announceInterval *= 10;
+            }
+        });
+    });
+    if ( theBible != undefined && theBible.xrefs != undefined ) {
+        console.log(theBible.xrefs);
+        console.log('There are a total of ', nXrefs, ' cross references loaded.');
+    }
+}
+
+// Let the real work begin...
+
+
+theBible = new Bible();
+console.log('theBible created.');
+theBible.LoadAll();
+
+data.db.close();
+
+console.log('Done.');
+
