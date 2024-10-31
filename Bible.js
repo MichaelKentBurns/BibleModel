aac4e871cf48cc93e7496eda4093ae2726eab04a
MichaelKentBurns <MichaelBurns@mac.com>
2024-10-31 16:36:43 -0500

    Still enhancing state machine, but Node.js not cooperating.

--- Bible.js
+++ Bible.js
@@ -12,13 +12,17 @@
 //                  specifically Judaism and Christianity.
 //      Books - a sequence of writings by various authors that constitutes the Holy Canon of a faith.
 //
-const traceBible = true;
-var state;
-let                             state_init = 1, state_loadBooks = 2, state_waitBooks = 3, state_booksLoaded = 4, state_saveBooks = 5, state_booksSaved = 6, state_whatsNext = 7;
-const stateNames = [ 'null', 'init',        'loadBooks',          'waitBooks',         'booksLoaded',         'saveBooks',         'booksSaved',         'whatsNext' ];
-var stateNext;
-var iteration;
-if ( state == undefined ){
+const traceBible = true;  // Set this true to have the inner workings of this class traced to console log.
+
+// - - - - - - - - - Issue #1, state machine works better
+// Setup state machine. 
+var state;      // state number, defined in next two lines.   Can simply increment to move to next state sequentially. 
+let                     state_init = 1, state_loadBooks = 2, state_waitBooks = 3, state_booksLoaded = 4, state_saveBooks = 5, state_booksSaved = 6, state_whatsNext = 7, state_shutdown = 8, state_abort = 9;
+const stateNames = [ 'null', 'init',         'loadBooks',         'waitBooks',         'booksLoaded',         'saveBooks',         'booksSaved',         'whatsNext',          'shutdown',         'abort' ];
+var stateNext;  // Set this from one state to indicate next state after current iteration.
+var iteration;  // Everytime we run through the whole state machine section, we increment this.  
+const iterationMax = 20;  // don't get stuck in a perpetual loop. 
+if ( state == undefined ){    // Initial state sets up the infrastructure. 
     state = state_init;
     iteration = 1;
     if ( traceBible ) console.log('Bible.js Initializing for the first time');
@@ -27,9 +31,11 @@ if ( state == undefined ){
     if ( traceBible ) console.log('Bible.js iteration ', iteration, ' state=', state, ':', stateNames[state] );
 }
 
-
+// - - - - - - - - - - - Resources needed .
+// Various resources needed are defined here, primarily parsing the preferences file. 
 const { readFileSync } = require('fs');
-const configPath = './BibleModel.json';
+let scheduler;    // not universally available.
+const configPath = './BibleModel-preferences.json';
 const configData = readFileSync(configPath , (error) => {
     if (error) {
       console.log('An error has occurred reading configuration', error);
@@ -39,99 +45,123 @@ const configData = readFileSync(configPath , (error) => {
   });
 const config = JSON.parse(configData);
 
+// - - - - - - - - - - Initialize other modules. 
 if ( traceBible ) console.log('Bible.js Initializing');
 let Book = require('./Book.js');
 // let Location = require('./Location.js');
 let Xref = require('./Xref.js');
 
-
+// utility function to pause this main event loop to allow asynch tasks to run
 function sleep(ms) {
+    if ( traceBible ) console.log('Bible.js sleep() for ',ms,' milliseconds.');
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let theBible;
+// - - - - Globals 
+let theBible;   // The Bible class is a singleton and the root of the whole data model.
 
 //- - - - - - - - - - - begin Class definition - - - - - - - - - - -
 class Bible {
     constructor() {
-        this.books = [];
-        this.booksComplete = false;
-        this.booksReadError = undefined;
-        this.bookInitialized = false;
-        this.xrefs = [];
-        this.xrefsComplete = false;
-        this.xrefInitialized = false;
-        this.bibleInitialized = false;
+        this.books = [];                  // array of the books of the Bible. 
+        this.booksComplete = false;       // true once the books have been loaded into array.
+        this.booksReadError = undefined;  // if it fails, this is why
+        this.bookInitialized = false;     // signal that books are ready.
+
+        this.xrefs = [];                  // array of cross references 
+        this.xrefsComplete = false;       // true when cross refs are all loaded 
+        this.xrefInitialized = false;     // they are ready to be used
+
+        this.bibleInitialized = false;    // basic setup is complete and ready for further requests.
     }
 
+    // called from book loading to add each book to the collection of books
     AddBook(book) {
         this.books.push(book);
-        if ( traceBible ) console.log('Book added: ' + book.name);
+        if ( traceBible ) console.log('Bible.js Book added: ' + book.name);
     }
 
+    // called from cross reference loading to add a new xref to the list
     AddXref(xref) {
         this.xrefs.push(xref);
-        if ( traceBible ) console.log('Xref added: ' + xref);
+        if ( traceBible ) console.log('Bible.js Xref added: ' + xref);
     }
 
+    // Called during Bible init to trigger loading of the various components. 
     LoadAll() {
-        if ( traceBible ) console.log('Loading books...');
+        // Books get loaded first 
+        if ( traceBible ) console.log('Bible.js LoadAll() Loading books...');
         this.booksComplete = false;
         Book.load(theBible);
 
-        console.log('Loading cross references...');
+        // Next cross references might be loaded, or may be deferred for lazy loads. 
+      //  console.log('Loading cross references...');
         // Xref.load(theBible);
-        if (theBible.xrefs != undefined) {
-            if ( traceBible ) console.log('There are ', theBible.xrefs.length, ' cross references');
-        }
+      //  if (theBible.xrefs != undefined) {
+      //      if ( traceBible ) console.log('There are ', theBible.xrefs.length, ' cross references');
+      //  }
     }
 // if ( traceBible ) console.log('Bible Class Defined.');
 }
 //- - - - - - - - - - -end Class definition - - - - - - - - - - -
 
+// = = = = = = = = = = = Begin state machine logic = = = = = = = = = = = = = = = =
+while ( state < state_shutdown ) 
+{
+// - - - - - - initialization state sets up the data structures for data loading
 if ( state == state_init ) {
+
+    // First, create the singleton Bible structure.  
     theBible = new Bible();
-    if ( traceBible )     console.log('theBible created.');
+    if ( traceBible )     console.log('Bible.js singleton theBible created.');
+    // Tell the book and xref classes about the singleton.
     Book.setBible(theBible);
     theBible.bookInitialized = true;
     Xref.setBible(theBible);
     theBible.xrefInitialized = true;
 
-    stateNext = 0;
-    state += 1;
+    stateNext = 0;   // set to 0 so normal incremental state progression works. 
+    state += 1;      // init complete, move on to next state. 
     if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state]);
 }
 
+// - - - - - - - - first loading state is to load the list of the books, not the contents just the list.
 if ( state == state_loadBooks ) {
-    if (traceBible) console.log('Loading books...');
-    theBible.LoadAll();
-    stateNext = 0;
-    state += 1;
-    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+    if (traceBible) console.log('Bible.js Requesting the loading of books...');
+    theBible.LoadAll();   // ask the Bible class to load the books. 
+    stateNext = state + 1;  // it will take time, so let it go to end of loop before bumping the state.
+    state = 0;         // Don't consider any other states until we have come around again. 
+    if ( traceBible )     console.log('Bible.js next go around, advance state to ', stateNext, stateNames[stateNext] );
 }
 
+// - - - utility function needed to check if books are loaded.  
+// This part is tricky in node.js   Issue #1 requires some interesting dancing around. 
 async function waitForBooks(){
     let timeoutCount = 10;
     waitBooksStop = setInterval(waitBooks, 1000);
     while ( !waitBooks() && timeoutCount > 0 && !theBible.booksComplete ) {
-        if ( traceBible ) console.log('Still waiting for books to be done loading, ', timeoutCount, ' seconds to go.');
+        if ( traceBible ) console.log('Bible.js Still waiting for books to be done loading, ', timeoutCount, ' seconds to go.');
         timeoutCount--;
         await new Promise( r=> setTimeout(r, 1000 ));
     }
 }
 
+// Still more dancing, this should get simplified as I learn more. 
 let waitBooksStop;
 function waitBooks() {
-    if ( traceBible ) console.log('Waiting for all Books. There are ', theBible.books.length, ' books loaded.');
-    if (theBible.booksComplete) {
-        if ( traceBible ) console.log('waitBooks: All books are loaded.');
+    // Check to see if the books are loaded yet or not. 
+    if ( traceBible ) console.log('Bible.js Waiting for all Books. There are ', theBible.books.length, ' books loaded.');
+    if (theBible.booksComplete) {   // We think they are ready, but needed to check. 
+        if ( traceBible ) console.log('waitBooks: theBible.booksComplete. All books are loaded.');
         clearInterval(waitBooksStop);
         stateNext = 0;
-        state += 1;
+        state += 1;     // If they are all loaded then move on to next state. 
         if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
-        console.log('Might as well try to save now...');
-        Book.saveAll(theBible);
-        console.log("Books saved??");
+
+        // This next bit will probably be a new state by itself. 
+        // console.log('Might as well try to save now...');
+        // Book.saveAll(theBible);
+        // console.log("Books saved??");
         stateNext = 0;
         state += 1;
         if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
@@ -141,28 +171,26 @@ function waitBooks() {
         return false;
 }
 
+// - - - - - - - After book loading is requested, this state calls above functions to check progress. 
 if ( state == state_waitBooks ) {
-    if (traceBible) console.log('Waiting for books to be done loading.')
+    if (traceBible) console.log('Bible.js Waiting for books to be done loading.')
     waitForBooks();
     if (traceBible) console.log('There are ', theBible.books.length, ' books loaded.');
     if (traceBible) console.log('We think they must be loaded.  Are they?')
     if (traceBible) console.log('Done.');
     stateNext = 0;
-//    state += 1;
-//    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
-
 }
 
-
+// - - - - - - - - - books all loaded, move on to next things. 
 if ( state == state_booksLoaded ) {
-    if (traceBible) console.log('Books are done loading.')
+    if (traceBible) console.log('Bible.js Books are done loading.')
 
     stateNext = 0;
     state += 1;
     if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
 }
 
-
+// - - - - - - - - after books loaded we want to snapshot to json, but also maybe later as well. 
 if ( state == state_saveBooks ) {
     if (traceBible) console.log('Saving books as json.')
 
@@ -174,7 +202,7 @@ if ( state == state_saveBooks ) {
     if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
 }
 
-
+// - - - - - - - - - 
 if ( state == state_booksSaved ) {
     if (traceBible) console.log('Now books are saved, now what?.')
     console.log("Books saved!");
@@ -184,18 +212,65 @@ if ( state == state_booksSaved ) {
     if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
 }
 
+// - - - - - - - - - Placeholder for future additions, but for now do nothing. 
+if ( state == state_whatsNext ) {
+    if (traceBible) console.log('Bible.js What is next?.')
+    stateNext = 0;
+    state += 1;
+    if ( traceBible )     console.log('Bible.js advance state to ', state, stateNames[state] );
+}
+
 this.bibleInitialized = true;
-if ( traceBible ) console.log('Bible.js Complete state=', state, ':', stateNames[state], ' stateNext=', stateNext, ':' , stateNames[stateNext]);
+if ( traceBible ) console.log('Bible.js Ready state=', state, ':', stateNames[state], ' stateNext=', stateNext, ':' , stateNames[stateNext]);
+
+// - - - State machine complete (all states considered in this iteration.) 
 if ( stateNext > 0 ) {
     state = stateNext;
 }
-else {
-    console. log('start nextTick');
-    process.nextTick(() => {
-        console. log('nextTick callback');
-        });
-    console. log('scheduled');
+
+
+if ( scheduler != undefined ) {
+// - - - - - - scheduler.yield is not universally available - - - 
+// at end of each iteration, yield to scheduled tasks.
+    if ( traceBible ) console.log('Bible.js yields.');
+    scheduler.yield();
+    }
+ else {
+        if ( traceBible ) console. log('start nextTick');
+        process.nextTick(() => {
+              if ( traceBible ) console.log('nextTick callback');
+            });
+        if ( traceBible ) console. log('scheduled');
+        sleep(1000);  // sleep this event loop.   
+        // break;   // should break the big while loop.
+      }
+
+
+if ( iteration > iterationMax ) {
+    if ( traceBible ) console.log('Bible.js has reached maximum iterations.  Time to quit.')
+    if ( state == state_shutdown )
+        state = state_abort;
+    else
+        state = state_shutdown;    
 }
+// done with this iteration, go around again...
+iteration += 1;
+if ( traceBible ) console.log('Bible.js iteration ', iteration, ' state=', state, ':', stateNames[state] );
+} // = = = = = = = = = = end of state machine loop. 
+
+
+// - - - - - - - - - Normal shutdown - - - - 
+if ( state == state_shutdown ) {
+   console.log('Bible.js complete, do normal shutdown.');
+   // Nothing needed yet. 
+}
+else if ( state == state_abort ) {
+    if ( traceBible ) console.log('Bible.js abnormal shutdown.');
+    process.abort();
+}
+
+
+// - - - - - - - - - - - - 
 
 
 module.exports = Bible, theBible;
