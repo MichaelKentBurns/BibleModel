2fdd82be4fa5efd3778ff1d8e0faf4a6a04d05ba
Michael Kent Burns <MichaelBurns@mac.com>
2025-02-06 19:51:29 +0000

    Changing Notes.html/mjs to use Note

--- DevUI/Notes.mjs
+++ DevUI/Notes.mjs
@@ -1,13 +1,9 @@
-import { Note } from "./Note.mjs";
-import { RestClient2 } from "./RestClient2.js";
+import { Note } from "../Note.mjs";
+// import { RestClient2 } from "./RestClient2.js";
 
-// let prefsData = RestClient2.callRestClient("/preferences");
-let prefsPromise;  // not used currently.
 const preferencesLocalStorageTag = "BibleModel.prefs";
 const preferences = JSON.parse(localStorage.getItem(preferencesLocalStorageTag) );
 if ( preferences != undefined ) console.log("Note: Preferences read from local storage: ", preferences);
-
-let notesList;
 
 // isUpdate and possibly others were never appropriately initialized.
 let isUpdate = false;
@@ -46,48 +42,51 @@ addBtn.addEventListener("click", (e) => {
         let aNote = new Note();
         aNote.setText(noteDesc);
         aNote.setTitle(noteTitle);
-     //   aNote.setAuthor(prefs.Owner);
+        aNote.setAuthor(prefserences.OwnerInitials);
 
         let dateObj = new Date(),
             month = months[dateObj.getMonth()],
             day = dateObj.getDate(),
             year = dateObj.getFullYear();
 
+        if (aNote.Created = null)
+            aNote.Created = dateObj;
+        aNote.Modified = dateObj;
 
         let noteInfo = {
-            title: noteTitle, description: noteDesc,
+            title: noteTitle, 
+            description: noteDesc,
             date: `${month} ${day} ${year}`
         }
+
         if(!isUpdate){
-            notes.push(noteInfo);
             bibleNotes.push(aNote);
         }else {
             isUpdate = false;
-            notes[UpdateId] = noteInfo
-            bibleNotes[UpdateId] = noteInfo;
+            bibleNotes[UpdateId] = aNote;
         }
-        localStorage.setItem(localStorageTag, JSON.stringify(notes));
+        localStorage.setItem(localStorageTag, JSON.stringify(bibleNotes));
         closeIcon.click()
         showNotes()
     }
 });
 
-const notes = JSON.parse(localStorage.getItem(localStorageTag) || "[]");
+const bibleNotes = JSON.parse(localStorage.getItem(localStorageTag) || "[]");
 
 function showNotes() {
     document.querySelectorAll(".note").forEach(note => note.remove())
-    notes.forEach((note, index) => {
+    bibleNotes.forEach((note, index) => {
         let liTag = `  <li class="note">
       <div class="details">
-          <p>${note.title}</p>
-          <span>${note.description}</span>
+          <p>${note.getTitle()}</p>
+          <span>${note.getText()}</span>
       </div>
       <div class="bottom-content">
           <span>${note.date}</span>
           <div class="settings">
             <i onclick="showMenu(this)" class="uil uil-ellipsis-h">...</i>
             <ul class="menu">
-                <li onclick="updateNote(${index}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+                <li onclick="updateNote(${index}, '${note.getTitle()}', '${note.getText()}')"><i class="uil uil-pen"></i>Edit</li>
                 <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i>Delete</li>
             </ul>
         </div>
@@ -97,22 +96,6 @@ function showNotes() {
     });
 };
 
-//prefsPromise.then((response) => {
-//    console.log(response);
-//    console.log(response.statusText)
-//    let responseData = response.json(response);
-//    let responseText = JSON.stringify(responseData);
-//    notesList = responseData;
-//    showNotes();
-//});
-//    .then((versionsResult) => {
-//        responseData = versionsResult;
-//        responseText = JSON.stringify(responseText);
-//    });
-
-// showNotes();
-
-// after callRestClient has been called and it gets to await,  the server sees request and control comes here.
 closeIcon.addEventListener("click", ()=>{
     titleTag.value = "";
     descTag.value = "";
@@ -131,8 +114,8 @@ function deleteNote(noteId) {
     let confirmDel = confirm("Are you sure you want to delete this item?");
     if (!confirmDel) return;
 
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
+    bibleNotes.splice(noteId, 1);
+    localStorage.setItem(localStorageTag, JSON.stringify(bibleNotes));
     showNotes();
 }
 
@@ -150,6 +133,3 @@ closeIcon.addEventListener("click", ()=>{
     titleTag.value = "";
     descTag.value = "";
 });
-
-
-// when the previous 5 calls execute, control from here goes to the .then in RestClient.mjs ???
